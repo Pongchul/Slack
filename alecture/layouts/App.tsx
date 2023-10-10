b1aa5031afd302babb136695c3dfc61bb961767b
pongchul <inchul7511@gmail.com>
2023-10-10 16:39:23 +0900

    First commit

---
+++ alecture/layouts/App.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import loadale from "@loadable/component";
+import { Redirect, Route, Switch } from "react-router";
+
+const SignUp = loadale(() => import("@pages/SignUp"));
+const LogIn = loadale(() => import("@pages/LogIn"));
+
+const App = () => {
+  return (
+    <Switch>
+      <Redirect exact path="/" to="/login" />
+      <Route path="/login" component={LogIn} />
+      <Route path="/signup" component={SignUp} />
+    </Switch>
+  );
+};
+
+export default App;
+
+// pages - 서비스 페이지
+// components - 짜잘한 컴포넌트
+// layouts - 공통 레이아웃

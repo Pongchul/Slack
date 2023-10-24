082f8b9a30b15e566bafe9ec3ed6277ab87b3d3a
pongchul <inchul7511@gmail.com>
2023-10-24 13:22:38 +0900

    Featuring : Add pages each logic!

--- alecture/pages/Channel/index.tsx
+++ alecture/pages/Channel/index.tsx
@@ -1,10 +1,30 @@
-import React from "react";
+import React, { useCallback } from "react";
 import { Container, Header } from "@pages/Channel/styles";
+import ChatList from "@components/ChatList";
+import chat from "@components/Chat";
+import useInput from "@hooks/useInput";
+import ChatBox from "@components/ChatBox";
 
 const Channel = () => {
+  const [chat, onChangeChat] = useInput("");
+  const onSubmitForm = useCallback((e) => {
+    e.preventDefault();
+  }, []);
+
   return (
     <Container>
       <Header> channel !</Header>
+      {/*<ChatList*/}
+      {/*  chatSections={chatSections}*/}
+      {/*  ref={scrollbarRef}*/}
+      {/*  setSize={setSize}*/}
+      {/*  isReachingEnd={isReachingEnd}*/}
+      {/*/>*/}
+      <ChatBox
+        chat={chat}
+        onSubmitForm={onSubmitForm}
+        onChangeChat={onChangeChat}
+      />
     </Container>
   );
 };

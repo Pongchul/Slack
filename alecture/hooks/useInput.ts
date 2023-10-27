994aa07f47b401b2a2ef30421fc1bc6af14b3066
pongchul <inchul7511@gmail.com>
2023-10-27 19:59:40 +0900

    Featuring : Add hook!

--- alecture/hooks/useInput.ts
+++ alecture/hooks/useInput.ts
@@ -1,16 +1,6 @@
-import {
-  Dispatch,
-  SetStateAction,
-  useCallback,
-  useState,
-  ChangeEvent,
-} from "react";
+import { Dispatch, SetStateAction, useCallback, useState, ChangeEvent } from 'react';
 
-type ReturnTypes<T> = [
-  T,
-  (e: ChangeEvent<HTMLInputElement>) => void,
-  Dispatch<SetStateAction<T>>
-];
+type ReturnTypes<T> = [T, (e: ChangeEvent<HTMLInputElement>) => void, Dispatch<SetStateAction<T>>];
 
 const useInput = <T>(initialData: T): ReturnTypes<T> => {
   const [value, setValue] = useState(initialData);
